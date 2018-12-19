cda86d5fd8f2b8ee887d59f941e7584d1186d120
danhuynh0803 <danhuynh0803@gmail.com>
2018-12-18 19:49:53 -0500

    Add more polish to main menu UI

---
+++ Assets/Scripts/Sampling.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sampling : MonoBehaviour
+{
+    public AudioClip music;
+    public float[] samples;
+    private AudioSource audioSource;
+    private float timer;
+    private float pause = 0.1f;
+    private float clipLoudness;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        samples = new float[1024];
+        music.GetData(samples, 0);
+
+    }
+
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+        if (timer >= pause)
+        {
+            pause = 0f;
+            audioSource.clip.GetData(samples, audioSource.timeSamples);
+            clipLoudness = 0f;
+            foreach (float sample in samples)
+            {
+                clipLoudness += Mathf.Abs(sample);
+            }
+            clipLoudness /= 1024;
+        }
+
+    }
+
+}
